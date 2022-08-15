060a21b717145e619fec6929eeec474576440a33
Billy Ponto <bpgponto@gmail.com>
2022-08-15 18:59:13 +0800

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
 
     // toggle navbar mobile
     $(".mobile-menu-button").each(function (_, navToggler) {
-        console.log('sdlfkj')
         var target = $(navToggler).data("target");
         $(navToggler).on("click", function () {
             $(target).animate({
